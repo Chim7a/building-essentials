2703696a577577329e8cbe4fec4f0fb88408cdcd
Chim7a <80280391+Chim7a@users.noreply.github.com>
2022-05-18 21:42:09 +0100

    Set up

---
+++ src/components/Views/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+// import DoorHome from '../../Assets/DoorHome.jpg';
+
+const Home = () => {
+  return (
+    <div className="home">
+        hello
+    </div>
+  )
+}
+
+export default Home;
