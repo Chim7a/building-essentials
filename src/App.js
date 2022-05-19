a2a71888db7c014c09ba82428389776ae8fc96ee
Chim7a <80280391+Chim7a@users.noreply.github.com>
2022-05-19 21:51:29 +0100

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Views/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+       <Routes>
+          <Route path="/checkout" element={<h1>checkout</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/" element={<Home />} />
+       </Routes>
+     </div> 
+    </Router>
   );
 }
 
