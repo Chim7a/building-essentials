a2d041bfd8bab8cdbae4d559aac1b7f62813509c
Chim7a <80280391+Chim7a@users.noreply.github.com>
2022-05-29 23:25:48 +0100

    Home page update

--- src/components/Views/Home/Home.js
+++ src/components/Views/Home/Home.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Navigation from '../Navigation/Navigation';
 import './Home.css';
 
-// import DoorHome from '../../Assets/DoorHome.jpg';
 
 const Home = () => {
   return (
     <div className="home">
         <Navigation />
+        <div className="home__container">
+          <div className="home__title">
+          <h1>Building<span className="home__header">Essentials</span></h1>
+          <p>Aesthetic at its peak </p>
+          </div>
+
+          <p> Importer and distributor of all kinds of buildings materials such as:
+             Armored doors, Turkish doors, German doors, Israeli doors, American steel doors,
+             PVC aluminum doors, French panel doors, China doors, tiles and marble 
+          </p>
+        </div>
     </div>
   )
 }
