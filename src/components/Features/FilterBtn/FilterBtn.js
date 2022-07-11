2b24b7ec60c942cac2a8fc7ef132afdc5017512e
Chim7a <80280391+Chim7a@users.noreply.github.com>
2022-07-11 23:16:51 +0100

    Styling update

--- src/components/Features/FilterBtn/FilterBtn.js
+++ src/components/Features/FilterBtn/FilterBtn.js
@@ -6,7 +6,6 @@ import Checkbox from '@mui/material/Checkbox';
 const FilterBtn = ({setRefiners}) => {
   return (
     <div className="filter__container">
-      <div className="filter">
       <FormControlLabel control={<Checkbox defaultChecked style={{color: "#ffffff"}} />} label="All" />
       <FormControlLabel control={<Checkbox defaultChecked style={{color: "#ffffff"}} />} label="China Doors" />
       <FormControlLabel control={<Checkbox defaultChecked style={{color: "#ffffff"}} />} label="Flush/hard wooden Doors" />
@@ -18,7 +17,6 @@ const FilterBtn = ({setRefiners}) => {
       <FormControlLabel control={<Checkbox defaultChecked style={{color: "#ffffff"}} />} label="Madrid Doors" />
       <FormControlLabel control={<Checkbox defaultChecked style={{color: "#ffffff"}} />} label="Tiles" />
       </div>
-    </div>
   )
 }
 
--- src/components/Views/About/About.js
+++ src/components/Views/About/About.js
@@ -6,23 +6,25 @@ const About = () => {
   return (
     <div className="about">
         <Navigation />
-        <p>Building Essentials is a company fully registered with the Nigerian CAC to carry-out legitimate business transactions and to offer related services.
+        <div className="about__info">
+          <p>Building Essentials is a company fully registered with the Nigerian CAC to carry-out legitimate business transactions and to offer related services.
 
-        The company was founded in 2021 and ever since  has grown into one of the foremost importer and distributor of quality and yet affordable wide range of household building materials and related services.
-        
-        Our products includes; Luxurious and Security doors for homes and offices, Tiles and marble of all kinds 
+          The company was founded in 2021 and ever since  has grown into one of the foremost importer and distributor of quality and yet affordable wide range of household building materials and related services.
 
-        Our services which are guaranteed to offer maximum customer/client satisfaction includes ;
+          Our products includes; Luxurious and Security doors for homes and offices, Tiles and marble of all kinds 
 
-        The Installation of Handrails.
-        Installation of Stamped Floor.
+          Our services which are guaranteed to offer maximum customer/client satisfaction includes ;
 
-        We represent some of the best brands in the world. 
+          The Installation of Handrails.
+          Installation of Stamped Floor.
 
-        Our Vision is to be the No. 1 destination for luxury, safe and affordable building materials in Africa.
+          We represent some of the best brands in the world. 
 
-        Our Mission is to help everyone own a luxury, safe and affordable home through the delivery of quality building materials from world class brands employing a motivated and skilled professionals driven by innovation and technology.
-        </p>
+          Our Vision is to be the No. 1 destination for luxury, safe and affordable building materials in Africa.
+
+          Our Mission is to help everyone own a luxury, safe and affordable home through the delivery of quality building materials from world class brands employing a motivated and skilled professionals driven by innovation and technology.
+          </p>
+        </div>
     </div>
   )
 }
