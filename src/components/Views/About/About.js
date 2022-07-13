a508c3476c58cf2bb3ed7f23632707154796c279
Chim7a <80280391+Chim7a@users.noreply.github.com>
2022-07-13 22:45:45 +0100

    Project list styling

--- src/components/Views/About/About.js
+++ src/components/Views/About/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './About.css';
 import CompanyHeader from '../../Features/CompanyHeader/CompanyHeader';
 import Navigation from '../Navigation/Navigation';
-
+import AboutData from './AboutData.json';
 
 const About = () => {
   return (
@@ -13,23 +13,10 @@ const About = () => {
             <CompanyHeader />
           </div>
           <div className="about__text">
-              <p>Building Essentials is a company fully registered with the Nigerian CAC to carry-out legitimate business transactions and to offer related services.
-
-              The company was founded in 2021 and ever since  has grown into one of the foremost importer and distributor of quality and yet affordable wide range of household building materials and related services.
-
-              Our products includes; Luxurious and Security doors for homes and offices, Tiles and marble of all kinds 
-
-              Our services which are guaranteed to offer maximum customer/client satisfaction includes ;
-
-              The Installation of Handrails.
-              Installation of Stamped Floor.
-
-              We represent some of the best brands in the world. 
-
-              Our Vision is to be the No. 1 destination for luxury, safe and affordable building materials in Africa.
-
-              Our Mission is to help everyone own a luxury, safe and affordable home through the delivery of quality building materials from world class brands employing a motivated and skilled professionals driven by innovation and technology.
-              </p>
+            {AboutData.aboutMessage.map((about, i ) => (
+              <p>{about.aboutText}</p>
+            ))}
+              
           </div>
         </div>
     </div>
